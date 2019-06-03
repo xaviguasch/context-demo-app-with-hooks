d9bc7c3f44ad67c69bb9f09ca464749d3184e013
xaviguasch <xaviguasch@gmail.com>
2019-06-03 10:48:09 +0200

    refactored the Language Provider to a hook

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,26 +1,14 @@
-import React, { Component, createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const LanguageContext = createContext()
 
-export class LanguageProvider extends Component {
-  constructor(props) {
-    super(props)
+export function LanguageProvider(props) {
+  const [language, setLanguage] = useState('spanish')
+  const changeLanguage = e => setLanguage(e.target.value)
 
-    this.state = { language: 'spanish' }
-    this.changeLanguage = this.changeLanguage.bind(this)
-  }
-
-  changeLanguage(e) {
-    this.setState({ language: e.target.value })
-  }
-
-  render() {
-    return (
-      <LanguageContext.Provider
-        value={{ ...this.state, changeLanguage: this.changeLanguage }}
-      >
-        {this.props.children}
-      </LanguageContext.Provider>
-    )
-  }
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {props.children}
+    </LanguageContext.Provider>
+  )
 }
