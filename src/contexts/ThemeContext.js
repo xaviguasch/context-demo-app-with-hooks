bf76003a12e1b7ae86f07d79dade84bd99643271
xaviguasch <xaviguasch@gmail.com>
2019-06-03 11:42:32 +0200

    added custom hook to ThemeContext

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,25 +1,14 @@
-import React, { Component, createContext } from 'react'
+import React, { createContext } from 'react'
+import useToggleState from '../hooks/useToggleState'
 
 export const ThemeContext = createContext()
 
-export class ThemeProvider extends Component {
-  constructor(props) {
-    super(props)
+export function ThemeProvider(props) {
+  const [isDarkMode, toggleTheme] = useToggleState(false)
 
-    this.state = { isDarkMode: false }
-
-    this.toggleTheme = this.toggleTheme.bind(this)
-  }
-
-  toggleTheme() {
-    this.setState({ isDarkMode: !this.state.isDarkMode })
-  }
-
-  render() {
-    return (
-      <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
-        {this.props.children}
-      </ThemeContext.Provider>
-    )
-  }
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  )
 }
